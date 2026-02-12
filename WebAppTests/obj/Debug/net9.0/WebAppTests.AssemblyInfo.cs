abb8610d49469ea69a57466e82147910598c3b59
Vicky Sharma <vkeysbox@gmail.com>
2026-02-12 18:59:33 +0100

    commit changes

--- WebAppTests/obj/Debug/net9.0/WebAppTests.AssemblyInfo.cs
+++ WebAppTests/obj/Debug/net9.0/WebAppTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b064ae8648bf3a7d02b86516942394d8a12d621f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
